0799ddca2696da2950030ba28a274f3aad73381f
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-02-01 13:43:20 +0530

    Ui pages like index,admin,cart,createProducts,owners-login,shop etc in ejs are created

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const db=require("./config/mongoose-connection")
 const ownersRouter =require("./routes/ownersRouter");
 const usersRouter =require("./routes/usersRouter");
 const productsRouter =require("./routes/productsRouter");
+const index=require("./routes/index");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 
+app.use("/", index)
 app.use("/owners", ownersRouter);   // owners releted rountes => routes/ownersRouter file madhe share zalet 
 app.use("/users", usersRouter);
 app.use("/products", productsRouter );
