99668310be50d517c6ea57dc65dd565215d3270d
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-02-03 22:26:11 +0530

    Add to Cart routes created

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,53 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 const isLoggedIn = require("../middlewares/isLoggedIn");
-const productModel= require("../models/product-model"); // Import the product model
+const productModel= require("../models/product-model"); 
+const userModel= require("../models/user-model");
 
 router.get("/", function (req, res) {
     let error = req.flash("error");
-    res.render("index", { error: "" });
+    let success=req.flash("success")
+    res.render("index", { error, loggedIn:false });
 });
 
 router.get("/shop",isLoggedIn, async function (req, res) {
-    try {
-            
-        const products = await productModel.find()
-
+    try {    
+        let products = await productModel.find()
+        let success=req.flash("success")
         // Pass products to the shop page
-        res.render("shop", { products });
+        res.render("shop", { products,success });
     } catch (error) {
         console.log(error);
         res.status(500).send("Server Error");
     }
 });
+
+router.get("/AddToCart/:productid", isLoggedIn, async function (req, res) {
+    try {    
+        let user = await userModel.findOne({ email: req.user.email }); // user find
+
+        let productId = req.params.productid.replace(":", ""); // Remove extra colon if present
+        
+        if (!mongoose.Types.ObjectId.isValid(productId)) {
+            req.flash("error", "Invalid product ID");
+            return res.redirect("/shop");
+        }// id is invalid
+
+        user.cart.push(new mongoose.Types.ObjectId(productId)); // Convert to ObjectId
+        await user.save();
+
+        req.flash("success", "Added to cart");
+        res.redirect("/shop");
+    } catch (error) {
+        console.log(error);
+        req.flash("error", "Something went wrong");
+        res.redirect("/shop");
+    }
+});
+
+
+ 
 
 
 module.exports = router;
